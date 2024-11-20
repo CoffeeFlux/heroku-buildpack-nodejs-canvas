d110e2b75b98bcbd5e3f9e440c9294c62e6caf3c
Ryan Lucia <ryan@luciaonline.net>
2024-11-20 15:04:25 -0800

    Update ci-setup.sh

--- etc/ci-setup.sh
+++ etc/ci-setup.sh
@@ -5,3 +5,5 @@ ci_repo_owner=${CIRCLE_PROJECT_USERNAME:-${GITHUB_REPOSITORY_OWNER}}
 [ "$ci_repo_owner" != "heroku" ] && echo "Run tests manually for forked PRs." && exit 0
 
 bundle exec hatchet ci:setup
+
+sudo apt-get install python build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev -y
